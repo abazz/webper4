b5a201912f96fb239d3c45c513d5dffb2627dd31
abazz <koivumaki.antti@gmail.com>
2017-11-10 11:14:52 +0200

    valmis

--- js/main-b.js
+++ js/main-b.js
@@ -1,38 +1,5 @@
 'use strict';
 
-// Tee funktio 'showImages', joka
-// lataa kuvat.json tiedoston, joka sisältää näytettävät kuvat taulukkona
-
-// tee silmukka joka lisää merkkijonoon (string) jokaisesta kuvasta alla olevan HTML:n
-
-
-// Silmukan jälkeen tulosta HTML-koodi (output) <ul>-elementin sisälle innerHTML:n avulla
-/*
-const showImages = () => {
-  fetch('kuvat.json').then((response) => {
-    return response.json();
-  }).then((json) => {
-    console.log(json);
-    let html = '';
-    json.forEach((kuva) => {
-      html += `<li>
-                  <figure>
-                      <a href="img/original/${kuva.mediaUrl}"><img src="img/thumbs/${kuva.mediaThumb}"></a>
-                      <figcaption>
-                          <h3>${kuva.mediaTitle}</h3>
-                      </figcaption>
-                  </figure>
-              </li>`;
-    });
-    const ul = document.querySelector('ul');
-    ul.innerHTML = html;
-  });
-};
-
-showImages();
-*/
-/*
-// sama tehtynä funktiolla jossa ei ole kovakoodausta
 const loadJSON = (url, func) => {
   fetch(url).then((response) => {
     return response.json();
@@ -40,6 +7,24 @@ const loadJSON = (url, func) => {
     func(json);
   });
 };
+
+const linkkiTapahtumat = () => {
+  const linkit = document.querySelectorAll('ul a');
+  // käy forEachillä linkit läpi
+  linkit.forEach((linkki) => {
+    // lisää jokaiseen linkkiin click event
+    linkki.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      // klikatessa hae linkin href arvo ja laita se modalin img:n src arvoksi
+      const href = linkki.getAttribute('href');
+      const img = modal.querySelector('img');
+      img.setAttribute('src', href);
+      // vaihda modalin luokaksi lightbox hiddenin sijaan
+      modal.classList.replace('hidden', 'lightbox');
+    });
+  });
+};
+
 
 const templateFunction = (json) => {
   let html = '';
@@ -55,57 +40,17 @@ const templateFunction = (json) => {
   });
   const element = document.querySelector('ul');
   element.innerHTML = html;
+  // linkit voi valita vasta tämän jälkeen, eli html on nyt valmis
+  linkkiTapahtumat();
 };
 
 loadJSON('kuvat.json', templateFunction);
 
-document.getElementById("default").addEventListener("click", function(event){
-    event.preventDefault();
-});*/
-
+const suljeNappi = document.querySelector('.closeBtn');
 const modal = document.querySelector('#modal');
-const img = modal.querySelector('img');
-const closeBtn = document.querySelector('.closeBtn');
-const ul = document.querySelector('ul');
 
-const showImages = () => {
-  fetch('kuvat.json').then((response) => {
-    return response.json();
-  }).then((json) => {
-    let html = '';
-    json.forEach((kuva) => {
-      html += `<li>
-                  <figure>
-                      <a href="img/original/${kuva.mediaUrl}"><img src="img/thumbs/${kuva.mediaThumb}"></a>
-                      <figcaption>
-                          <h3>${kuva.mediaTitle}</h3>
-                      </figcaption>
-                  </figure>
-              </li>`;
-    });
-    
-    ul.innerHTML = html;
-    init();
-  });
-};
-showImages();
-
-
-const init = () => {
-  const linkit = ul.querySelectorAll('a');
-  linkit.forEach((linkki) => {
-    linkki.addEventListener('click', (evt) => {
-      evt.preventDefault();
-    
-      img.setAttribute('src', evt.currentTarget.href);
-      modal.classList.replace('hidden', 'lightbox');
-      modal.style = 'top: '+window.scrollY+'px';
-    });
-  });
-};
-
-
-closeBtn.addEventListener('click', (evt) => {
+suljeNappi.addEventListener('click', (evt) => {
   evt.preventDefault();
+  // vaihda modalin luokka lightboxista hiddeniin
   modal.classList.replace('lightbox', 'hidden');
 });
